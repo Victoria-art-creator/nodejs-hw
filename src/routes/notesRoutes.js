4582dd2856941dfc85a6482fcb5e734d0229e998
Victoria <victoria.vozyanovq@gmail.com>
2026-01-31 00:22:10 +0200

    add files

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
 import {
   createNote,
   deleteNote,
@@ -12,9 +13,11 @@ import {
   createNoteSchema,
   updateNoteSchema,
 } from '../validations/notesValidation.js';
-import { celebrate } from 'celebrate';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
+
+router.use('/notes', authenticate);
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import cors from 'cors';
 import helmet from 'helmet';
 import 'dotenv/config';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const PORT = process.env.PORT ?? 3000;
 const app = express();
@@ -16,7 +18,9 @@ app.use(logger);
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(cookieParser);
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
